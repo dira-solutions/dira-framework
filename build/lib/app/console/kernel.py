658f2a9712060bfafbfc68310069d76ed7f2c1b5
drunk_rabbit <32393133+drunkrabbit1@users.noreply.github.com>
2024-05-21 19:39:45 +0300

    Delete build/lib directory

--- build/lib/app/console/kernel.py
+++
@@ -1,6 +0,0 @@
-from diracore.foundation.console.console_kernel import ConsoleKernel
-import os
-
-class Kernel(ConsoleKernel):
-    def commands(self) -> None:
-        self.load('app.console.commands.*')
